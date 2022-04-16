52c9cb8dd8f3a22b8b20efb743ecc580a7e824c9
Abhinav <abhinavgargshukla@gmail.com>
2022-04-16 12:29:30 +0530

    #tags corrected added actionTypes

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,11 @@
+import { FETCH_ALL,LIKE,CREATE,UPDATE,DELETE } from "../constants/actionTypes";
 import * as api from "../api";
 
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
     dispatch({
-      type: "FETCH_ALL",
+      type: FETCH_ALL,
       payload: data,
     });
   } catch (error) {
@@ -16,7 +17,7 @@ export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
     dispatch({
-      type: "CREATE",
+      type: CREATE,
       payload: data,
     });
   } catch (error) {
@@ -28,7 +29,7 @@ export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post);
 
-    dispatch({ type: 'UPDATE', payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error);
 
@@ -38,7 +39,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost=(id)=>async(dispatch)=>{
   try{
     await api.deletePost(id);
-    dispatch({type:'DELETE',payload:id});
+    dispatch({type:DELETE,payload:id});
   }
   catch(error){
     console.log(error);
@@ -49,7 +50,7 @@ export const deletePost=(id)=>async(dispatch)=>{
 export const likePost=(id)=>async(dispatch)=>{
   try{
        const {data}=await api.likePost(id);
-       dispatch({type:'LIKE',payload:data});
+       dispatch({type:LIKE,payload:data});
        console.log('dispatched');
   }catch(error){
     console.log(error);
