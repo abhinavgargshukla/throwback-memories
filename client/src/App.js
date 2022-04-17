84ba07f35db0c8d144a1e48af17e76d9722d5b5c
Abhinav <abhinavgargshukla@gmail.com>
2022-04-17 22:54:09 +0530

    sign in and sign up form added

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,28 @@
-import React,{useEffect,useState} from "react";
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from "react-redux";
+import React from 'react';
+import { Container} from '@material-ui/core';
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 import './index.css';
 
-import {getPosts} from './actions/posts';
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
-import useStyles from './styles';
+import Home from './components/Home/Home';
+import Navbar from "./components/Navbar/Navbar";
+import Auth from './components/Auth/Auth';
 
 const App = () => {
-  const[currentId,setCurrentId]=useState(null);
-  const classes=useStyles();
-  const dispatch=useDispatch();
-
-  useEffect(()=>{
-    dispatch(getPosts());
-  },[currentId,dispatch])
+ 
   return (
-    <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading}  variant="h2" align="center">
-          Memories
-        </Typography>
-        <img className={classes.image} src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9n4iuf7Imt8sIkVO5WMl7NhHiKLJ1us8e2Q&usqp=CAU" alt="memories" height="60"/> 
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container  justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId}/>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId}/>
-            </Grid>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+      <Navbar/>
+         <Routes>
+             <Route path="/" exact element={<Home/>}/>
+              <Route path="/auth" exact element={<Auth/>}/>
 
-          </Grid>
-        </Container>
-      </Grow>
+
+        </Routes>
+      
       
     </Container>
+    </BrowserRouter>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -3,17 +3,21 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from 'dotenv';
 
 const app = express();
 
-
+dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts',postRoutes);
-const CONNECTION_URL =
-  "mongodb+srv://abhinavshukla:abhi123@cluster0.mo0cc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+app.get('/',(req,res)=>{
+  res.send('Hello to memories API');
+})
 const PORT = process.env.PORT || 5000;
+const CONNECTION_URL="mongodb+srv://abhinavshukla:abhi123@cluster0.mo0cc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
